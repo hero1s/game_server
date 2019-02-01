496ad81ae0250d670541ddd02d9c550f9a6e37cb
toney <toney@macdeMacBook-Pro.local>
2019-02-01 09:39:10 +0800

    提交common目录

---
+++ common/cs_msg/pbcreate.bat
@@ -0,0 +1,3 @@
+..\tools\protoc_lua --descriptor_set_out=./lua/cs_msg.pb --plugin=protoc-gen-lua  ./*.proto
+
+pause
---
+++ common/cs_msg/pbcreate.sh
@@ -0,0 +1,1 @@
+protoc --descriptor_set_out=./lua/cs_msg.pb --plugin=protoc-gen-lua  ./*.proto
