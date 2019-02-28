1531b557e4c9635d5a8bfcdb6cd7624fd82f56d9
toney <toney@macdeMacBook-Pro.local>
2019-02-28 21:07:41 +0800

    websocket放入底层网络库

--- server/server_frame/src/network/Session.h
+++ server/server_frame/src/network/Session.h
@@ -5,6 +5,7 @@
 #include "SocketOpt.h"
 #include "TLock.h"
 #include "Message.h"
+#include <string>
 
 namespace Network
 {
@@ -35,6 +36,31 @@ class Session : public LinkD_T<Session>
 	friend void* io_thread(void* param);
 	friend class SessionPool;
 
+    enum WS_FrameType {
+        WS_EMPTY_FRAME = 0xF0,
+        WS_ERROR_FRAME = 0xF1,
+        WS_TEXT_FRAME = 0x01,
+        WS_BINARY_FRAME = 0x02,
+        WS_PING_FRAME = 0x09,
+        WS_PONG_FRAME = 0x0A,
+        WS_OPENING_FRAME = 0xF3,
+        WS_CLOSING_FRAME = 0x08
+    };
+private://websocket
+    void ShakeHandsHandle(const char *buf, int buflen);
+
+    bool FindHttpParam(const char *param, const char *buf);
+
+    int wsDecodeFrame(std::string inFrame, std::string &outMessage);
+
+    int wsEncodeFrame(std::string inMessage, std::string &outFrame, enum WS_FrameType frameType);
+
+    int32_t GetWebSocketPacketLen(const uint8_t* pData, uint16_t wLen);
+
+private:
+    bool shake_hands_;//是否已经握手
+
+
 public:
 	/// dwTimeOut .
 	Session(uint32_t dwSendBufferSize, uint32_t dwRecvBufferSize, uint32_t dwMaxPacketSize, uint32_t dwTimeOut);
@@ -46,7 +72,7 @@ public:
 	void Init();
 	bool Send(uint8_t* pMsg, uint16_t wSize);
 
-	int OnSend();
+	bool OnSend();
 	int DoSend(IoHandler* pIoHandler);
 	int DoRecv();
 	int PreSend(IoHandler* pIoHandler);
@@ -57,6 +83,8 @@ public:
 	bool HandleRecvMessage();
 	//解码消息到消息队列
 	bool DecodeMsgToQueue();
+	//解码websocket消息到队列
+	bool DecodeWebSocketToQueue();
 
 	void BindNetworkObject(NetworkObject* pNetworkObject);
 	void UnbindNetworkObject();
