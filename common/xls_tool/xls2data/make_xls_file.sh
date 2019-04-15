96588ccbc14daf7a5812e878cc47239ed9d0c9ae
toney <toney@macdeMacBook-Pro.local>
2019-04-15 21:11:55 +0800

    添加工具脚本，生成cpp代码

---
+++ common/xls_tool/xls2data/make_xls_file.sh
@@ -0,0 +1,5 @@
+
+find ./proto -name "*.proto" | xargs protoc -I=./proto/ --cpp_out=./gen_cpp/
+
+cp ./gen_cpp/*.*   ../../../server/servers/pb/src/
+cp ./txt/*.* 	   ../../../server/release/server_config/scp/
