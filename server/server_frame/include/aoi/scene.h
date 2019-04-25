592d14812c680ba28f141b6a60e20717da28d453
toney <toney@macdeMacBook-Pro.local>
2019-04-25 09:21:56 +0800

    添加对象指针

--- server/server_frame/include/aoi/scene.h
+++ server/server_frame/include/aoi/scene.h
@@ -6,6 +6,7 @@
 #include <stdio.h>
 #include <memory>
 #include <unordered_map>
+#include <functional>
 #include "quad_tree.hpp"
 #include "utility/comm_macro.h"
 
@@ -23,6 +24,7 @@ namespace aoi {
         float view_h = 20.0f;
         entity_view_t view;
         bool need_check_view = true;//是否需要视野检测(静态物品不需要)
+        math::objectid_t view_uid;
 
         virtual void view_event(std::vector<math::objectid_t> &enter_list, std::vector<math::objectid_t> &leave_list) {}
     };
@@ -37,9 +39,9 @@ namespace aoi {
 
         virtual ~CScene() {}
 
-        void insert(math::objectid_t uid, float x, float y) {
-            qtree->insert(uid, x, y);
-            scene_objs.try_emplace(uid, CSceneObj());
+        void insert(CSceneObj* pObj, float x, float y) {
+            qtree->insert(pObj->view_uid, x, y);
+            scene_objs.try_emplace(pObj->view_uid,pObj);
             is_need_check = true;
             check_aoi();
         }
@@ -58,61 +60,62 @@ namespace aoi {
 
         void check_aoi() {
             if (is_need_check) {
-                for (auto &p : scene_objs) {
-                    if(!p.second.need_check_view)continue;
+                for (auto [uid,pObj] : scene_objs) {
+                    if(!pObj->need_check_view)continue;
                     query_result.clear();
-                    qtree->query(p.first, p.second.view_w, p.second.view_h, query_result);
-                    compare_view(p.first, p.second, query_result);//第一次遍历，更新版本号
-                    process_view_event(p.first, p.second);//第一次遍历打印结果
+                    qtree->query(uid, pObj->view_w, pObj->view_h, query_result);
+                    compare_view(uid, pObj, query_result);//第一次遍历，更新版本号
+                    process_view_event(uid, pObj);//第一次遍历打印结果
                 }
                 is_need_check = false;
             }
         }
 
     protected:
-        void compare_view(math::objectid_t uid, CSceneObj &p, const std::vector<math::objectid_t> &query_result) {
-            p.version += 2;
+        void compare_view(math::objectid_t uid, CSceneObj* pObj, const std::vector<math::objectid_t> &query_result) {
+            pObj->version += 2;
             for (auto &otherid : query_result) {
                 if (otherid == uid) {
                     continue;
                 }
-                auto[iter, success] = p.view.try_emplace(otherid, p.version);//新进入玩家
+                auto[iter, success] = pObj->view.try_emplace(otherid, pObj->version);//新进入玩家
                 if (!success)//玩家已经在视野内
                 {
-                    iter->second = p.version - 1;//更新已经在视野内玩家的版本号,比当前版本号小1
+                    iter->second = pObj->version - 1;//更新已经在视野内玩家的版本号,比当前版本号小1
                 }
             }
         }
 
-        void process_view_event(math::objectid_t uid, CSceneObj &p) {
+        void process_view_event(math::objectid_t uid, CSceneObj* pObj) {
             enter_list.clear();
             leave_list.clear();
-            for (auto &v : p.view) {
-                if (v.second == p.version) {
+            for (auto &v : pObj->view) {
+                if (v.second == pObj->version) {
                     //进入视野
                     LOG_DEBUG("player {} enter {} view", v.first, uid);
                     enter_list.emplace_back(v.first);
-                } else if (v.second != (p.version - 1))//出视野
+                } else if (v.second != (pObj->version - 1))//出视野
                 {
                     LOG_DEBUG("player {} leave {} view", v.first, uid);
                     //注意这里要从 p.view 删除已经出视野的玩家
                     leave_list.emplace_back(v.first);
                 }
             }
-            p.view_event(enter_list, leave_list);
+            if(!enter_list.empty() || !leave_list.empty()) {
+                pObj->view_event(enter_list, leave_list);
+            }
             for (auto &it:leave_list) {
-                p.view.erase(it);
+                pObj->view.erase(it);
             }
         }
 
     protected:
         std::shared_ptr<math::quad_tree<36>> qtree;
-        std::unordered_map<math::objectid_t, CSceneObj> scene_objs;
+        std::unordered_map<math::objectid_t, CSceneObj*> scene_objs;
         std::vector<math::objectid_t> query_result;
         std::vector<math::objectid_t> enter_list;
         std::vector<math::objectid_t> leave_list;
         bool is_need_check;
-
     };
 };
 
