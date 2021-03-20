69f123a6a1849936a4e1fa7b87685b956606e64c
toney <8242117@qq.com>
2021-03-20 20:54:12 +0800

    测试通过

--- server/release/server_config/server_config.lua
+++ server/release/server_config/server_config.lua
@@ -3,7 +3,8 @@
 --************ 服务器配置
 --*********************************************
 --数据库配置(改成只使用一个数据库地址toney)
-local database_config = { ip = "192.168.3.110", port = 8308, user = "root", passwd = "e23456" };
+--local database_config = { ip = "192.168.3.110", port = 8308, user = "root", passwd = "e23456" };
+local database_config = { ip = "127.0.0.1", port = 3306, user = "root", passwd = "e23456" };
 local database_dbname = {}
 database_dbname[0] = "chess";
 --database_dbname[1] = "chess_sysdata";
@@ -11,8 +12,8 @@ database_dbname[0] = "chess";
 --database_dbname[3] = "chess_log";
 
 --redis配置
---local redis_config = { host = "139.199.209.147",port = 13000,passwd="e2345" };
-local redis_config = { host = "192.168.3.110",port = 8379,passwd="e2345" };
+local redis_config = { host = "127.0.0.1",port = 6379,passwd="e2345" };
+--local redis_config = { host = "192.168.3.110",port = 8379,passwd="e2345" };
 
 --全局配置信息
 server_config =
